c04d1aec6601ea9903a4964a1b479e293cf3235d
DevYuns <ydhun93@gmail.com>
2023-02-10 17:56:07 +0900

    fix: remove invariant from [PostPage] due to build fail

--- app/[lang]/posts/[id]/page.tsx
+++ app/[lang]/posts/[id]/page.tsx
@@ -3,7 +3,6 @@ import type {Locale} from '~/lib/i18n';
 import type {Post} from '@prisma/client';
 import type {ReactElement} from 'react';
 import {getTranslates} from '~/lib/utils/getTranslation';
-import invariant from 'tiny-invariant';
 import {prismaClient} from '~/lib/prisma';
 
 async function getPost(id: string): Promise<Post | null> {
@@ -22,16 +21,14 @@ export default async function PostPage({
     getPost(id),
   ]);
 
-  invariant(data, 'Can not find the Post with provided id.');
-
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-h1 mb-4">
-          {post.title}: {data.title}
+          {post.title}: {data?.title}
         </h1>
         <p>
-          {post.content}: {data.content}
+          {post.content}: {data?.content}
         </p>
       </div>
       <Link href="../" className="mt-8 border-[1px] border-solid p-2 rounded">
