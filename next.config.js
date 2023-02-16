97504850e46531c89611709404ecca3812401083
DevYuns <ydhun93@gmail.com>
2023-02-16 12:19:50 +0900

    build: setup  deploy script with github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const onGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix,
+  basePath,
   swcMinify: true,
   reactStrictMode: true,
   experimental: {
